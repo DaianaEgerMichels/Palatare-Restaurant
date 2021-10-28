10239cfbba27ed85560e04329f49cef278804df7
Daiana Eger Michels <daianaem.12@gmail.com>
2021-10-28 12:20:37 -0300

    Atualizar páginas Home e PageTwo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,16 +3,21 @@ import Header from "../../components/Header";
 import "./style.css";
 import logo from "../../components/Header/chefe.jpg"
 import Main from "../../components/Main";
+import Search from "../../components/Search";
+import Footer from "../../components/Footer";
 
 class Home extends React.Component{
     render(){
         return(
-            <>
+            
             <div className="home">
-                <Header src={logo} alt={"chefe"} name={"Palatare Restaurant"} title={"Welcome to the Palatare Restaurant home page!"}></Header>
-                <Main></Main>
+                <Header src={logo} alt={"chefe"} name={"Palatare Restaurant"} title={"Welcome to the Palatare Restaurant page!"}></Header>
+                <Main>
+                    <Search type={'text'} placeholder={'Type here what you are looking for...'}></Search>
+                </Main>
+                <Footer></Footer>
             </div>
-            </>
+            
         )
     }
 }
--- src/pages/PageTwo/index.js
+++ src/pages/PageTwo/index.js
@@ -3,18 +3,20 @@ import Header from "../../components/Header";
 import "./style.css";
 import logo from "../../components/Header/chefe.jpg"
 import Main from "../../components/Main";
+import Footer from "../../components/Footer";
 
-class Home extends React.Component{
+class PageTwo extends React.Component{
     render(){
         return(
-            <>
+        
             <div className="page-two">
-                <Header src={logo} alt={"chefe"} name={"Palatare Restaurant"} title={"Welcome to the Palatare Restaurant revenues page!"}></Header>
+                <Header src={logo} alt={"chefe"} name={"Palatare Restaurant"} title={"Palatare Restaurant revenues page"}></Header>
                 <Main></Main>
+                <Footer></Footer>
             </div>
-            </>
+            
         )
     }
 }
 
-export default Home;
+export default PageTwo;
