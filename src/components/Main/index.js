bab10eefe760d5abc9389a23c7e0fca9325a78b5
Daiana Eger Michels <daianaem.12@gmail.com>
2021-10-28 12:18:24 -0300

    Correção das tags do componente Main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,13 +1,11 @@
 import React from "react";
-import Search from "../Search";
 import "./style.css"
 
 class Main extends React.Component{
     render(){
         return(
-            <div className="main">
-            <Search type={'text'} placeholder={'Type here what you are looking for...'}></Search>
-            </div>
+            <main className="main">
+            </main>
         )
     }
 }
