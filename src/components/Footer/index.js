f6d3883850b5fb0b0440263adae3c405c5fe34f9
Daiana Eger Michels <daianaem.12@gmail.com>
2021-10-28 12:17:28 -0300

    Criar componente footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css"
+
+class Footer extends React.Component{
+    render(){
+        return(
+                <footer className="footer">
+                    <h3>Save and gain health!</h3>
+                    <p>Contato: palatare@palatare.com</p>
+                </footer>
+        )
+    }
+}
+
+export default Footer;
